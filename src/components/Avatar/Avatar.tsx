9e4441064e652e7266ebf5a387882c5b5e4807ec
ng <n.guerns@gmail.com>
2024-08-07 12:17:43 -0500

    chore(ui): avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+import { useMemo } from "react";
+
+type Props = {
+  path: string;
+  size?: "small" | "medium" | "large" | "xl";
+  className?: string;
+};
+
+export default function Avatar({ path, size = "medium", className }: Props) {
+  const dimensions = useMemo(() => {
+    switch (size) {
+      case "xl":
+        return 150;
+      case "large":
+        return 75;
+      case "medium":
+        return 50;
+      case "small":
+      default:
+        return 25;
+    }
+  }, [size]);
+
+  return (
+    <div
+      className={clsx("inline-block rounded-full bg-slate-50", className)}
+      style={{
+        width: `${dimensions}px`,
+        height: `${dimensions}px`,
+      }}
+    >
+      <img
+        src={path}
+        height={dimensions}
+        width={dimensions}
+        className="w-full h-full rounded-full object-fit"
+      />
+    </div>
+  );
+}
