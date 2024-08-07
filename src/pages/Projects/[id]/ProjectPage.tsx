ef6c980244664c919956f6da8343ac8595c0d71c
ng <n.guerns@gmail.com>
2024-08-07 14:07:32 -0500

    chore(pages): added projects page and content

--- src/pages/Projects/[id]/ProjectPage.tsx
+++ src/pages/Projects/[id]/ProjectPage.tsx
@@ -1,30 +1,73 @@
 import { useLoaderData } from "react-router-dom";
 import { Project } from "../../../@types";
 import { ExternalLink } from "lucide-react";
+import Avatar from "../../../components/Avatar/Avatar";
 
 export default function ProjectPage() {
   const project = useLoaderData() as Project;
 
   return (
-    <div className="bg-white rounded border p-3">
-      <h1 className="font-display font-bold text-4xl text-slate-700">
-        {project.title}
-      </h1>
-      <p>{project.blurb}</p>
-      <a
-        href={project.url}
-        target="_blank"
-        className="inline-flex items-center gap-x-1 bg-white rounded border px-2 py-1"
-      >
-        Visit site
-        <sup>
-          <ExternalLink className="w-3 text-slate-500" />
-        </sup>
-      </a>
+    <div className="bg-white rounded border p-4 md:p-8">
+      <header className="mb-4">
+        <Avatar path={project.logo} alt={project.title} />
 
-      <section>Image goes here</section>
+        <h1 className="font-display font-bold text-4xl text-slate-700">
+          {project.title}
+        </h1>
 
-      <section>Technologies go here</section>
+        <p className="text-slate-500">{project.blurb}</p>
+
+        <a
+          href={project.url}
+          target="_blank"
+          className="inline-flex items-center gap-x-1 bg-slate-700 hover:bg-slate-800 text-white rounded px-2 py-1 mt-4"
+        >
+          Visit site
+          <ExternalLink className="w-3 text-slate-50" />
+        </a>
+      </header>
+
+      <section className="text-slate-600 mb-4">
+        {project.details?.map((detail, index) => (
+          <p key={index} className="mb-2">
+            {detail}
+          </p>
+        ))}
+      </section>
+
+      <section className="mb-4">
+        {project.images?.map((image, index) => (
+          <picture key={index}>
+            <source media="(max-width: 767px)" srcSet={image.mobile.jpeg} />
+            <source media="(max-width: 767px)" srcSet={image.mobile.webp} />
+            <source media="(min-width: 768px)" srcSet={image.tablet.jpeg} />
+            <source media="(min-width: 768px)" srcSet={image.tablet.webp} />
+
+            <img
+              src={image.tablet.jpeg}
+              alt={image.alt}
+              className="rounded-md mb-4"
+            />
+          </picture>
+        ))}
+      </section>
+
+      <section className="p-4 md:p-8 bg-slate-100 rounded mb-4">
+        <h2 className="font-display text-2xl mb-3 text-slate-700">
+          Technologies Used
+        </h2>
+
+        <div className="flex flex-wrap gap-2">
+          {project.technologies?.map((tech) => (
+            <span
+              key={tech}
+              className="bg-slate-500 text-white rounded-full px-3 py-1 text-sm"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,18 @@ export const BIO: Bio = {
   profile: "/assets/profile.jpeg",
   jobStatus: 'available',
   jobTitle: 'Software Engineer',
+  yearsExperience: 3,
+  jobLocations: [
+    'Knoxville',
+    'Denver',
+    'Colorado Springs',
+    'Seattle',
+    'Los Angeles',
+    'San Francisco',
+    'Tulsa',
+    'Charlotte',
+    'Raleigh',
+  ],
   socials: {
     linkedin: {
       label: "Let's connect",
@@ -33,30 +45,183 @@ export const BIO: Bio = {
   ],
   projects: [
     {
+      logo: "/assets/pnp_logo.svg",
       slug: 'project-national-parks',
       url: "https://projectnationalparks.com",
       title: 'Project National Parks',
       blurb: 'Explore the natural beauty of the National Parks',
+      details: [
+        'This project was built to showcase the incredible park system available to everyone. This website serves as a resource for people to learn, discover, and plan their visits to the National Parks.',
+        'With this project I wanted to practice a few concepts, namely test-driven development and continuously deployment. All unit tests were written with Vitest and React Testing Library. End-to-end tests were done using Cypress. This project was continuously deployed with Vercel and GitHub.'
+      ],
       technologies: [
-        "TypeScript", "React", "NextJS", "TailwindCSS"
+        "React", "NextJS", "Leaflet", "TailwindCSS", "Vitest", "Cypress", "React Testing Library", "GitHub", "Vercel", "CSS", "HTML", "JavaScript"
+      ],
+      images: [
+        {
+          alt: "Project National Parks landing page",
+          mobile: {
+            webp: "/assets/pnp_landing_420w.webp",
+            jpeg: "/assets/pnp_landing_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/pnp_landing_768w.webp",
+            jpeg: "/assets/pnp_landing_768w.jpg",
+          }
+        },
+        {
+          alt: "Project National Parks map to explore all parks",
+          mobile: {
+            webp: "/assets/pnp_explore_420w.webp",
+            jpeg: "/assets/pnp_explore_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/pnp_explore_768w.webp",
+            jpeg: "/assets/pnp_explore_768w.jpg",
+          }
+        },
+        {
+          alt: "Project National Parks explore map popup card of park",
+          mobile: {
+            webp: "/assets/pnp_popout_420w.webp",
+            jpeg: "/assets/pnp_popout_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/pnp_popout_768w.webp",
+            jpeg: "/assets/pnp_popout_768w.jpg",
+          }
+        },
+        {
+          alt: "Project National Parks page of a national park",
+          mobile: {
+            webp: "/assets/pnp_park_420w.webp",
+            jpeg: "/assets/pnp_park_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/pnp_park_768w.webp",
+            jpeg: "/assets/pnp_park_768w.jpg",
+          }
+        },
       ]
     },
     {
+      logo: "/assets/ps_logo.svg",
       slug: 'power-searches',
       url: "https://powersearches.org",
-      title: 'Power Searches',
-      blurb: 'Find the information you want with curated search shortcuts.',
+      title: 'Power Search',
+      blurb: 'Find the information you want with curated search shortcuts',
+      details: [
+        "This project was an exercise in studying the UI for Kagi's search engine. I liked the idea that you can specify where you want to search for content on the Internet.",
+        'For this project, I wanted to create a local-first approach to let users save search shortcuts for their needs. If you discover a website, repository, or API that searches a subset of content you can save the URL and pass in parameters to search for targeted content.',
+      ],
       technologies: [
-        "TypeScript", "React", "NextJS", "Vercel", "TailwindCSS"
+        "TypeScript", "React", "NextJS", "Vercel", "TailwindCSS", "RadixUI", "CSS", "HTML", "JavaScript"
+      ],
+      images: [
+        {
+          alt: "Power Search landing page",
+          mobile: {
+            webp: "/assets/ps_landing_420w.webp",
+            jpeg: "/assets/ps_landing_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ps_landing_768w.webp",
+            jpeg: "/assets/ps_landing_768w.jpg",
+          }
+        },
+        {
+          alt: "Power Search search dropdown menu",
+          mobile: {
+            webp: "/assets/ps_search_menu_420w.webp",
+            jpeg: "/assets/ps_search_menu_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ps_search_menu_768w.webp",
+            jpeg: "/assets/ps_search_menu_768w.jpg",
+          }
+        },
+        {
+          alt: "Power Search settings page",
+          mobile: {
+            webp: "/assets/ps_settings_420w.webp",
+            jpeg: "/assets/ps_settings_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ps_settings_768w.webp",
+            jpeg: "/assets/ps_settings_768w.jpg",
+          }
+        },
       ]
     },
     {
+      logo: "/assets/ddu_logo.svg",
       slug: 'down-down-up',
       url: "https://downdownup.pages.dev/",
       title: 'Down Down Up',
       blurb: 'A game to test your keyboard skills',
+      details: [
+        'I had been developing in Vue 2 for a couple years and recently Vue 3 was released. What better way to learn the new composition API than developing a small web game!',
+        "Down Down Up is a game to tests how quickly a user can type out the correct combination of keyboard arrows. I also added a secret game mode to test the user's ability to consecutively hit combos. Every incorrect entry reduces your existing time, adding an element of urgency for accuracy."
+      ],
       technologies: [
-        "TypeScript", "Vue",
+        "TypeScript", "Vue", "CSS", "HTML", "JavaScript"
+      ],
+      images: [
+        {
+          alt: "Down Down Up landing page",
+          mobile: {
+            webp: "/assets/ddu_landing_420w.webp",
+            jpeg: "/assets/ddu_landing_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ddu_landing_768w.webp",
+            jpeg: "/assets/ddu_landing_768w.jpg",
+          }
+        },
+        {
+          alt: "Down Down Up default keys",
+          mobile: {
+            webp: "/assets/ddu_keys_420w.webp",
+            jpeg: "/assets/ddu_keys_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ddu_keys_768w.webp",
+            jpeg: "/assets/ddu_keys_768w.jpg",
+          }
+        },
+        {
+          alt: "Down Down Up keys in active mode",
+          mobile: {
+            webp: "/assets/ddu_keys_active_420w.webp",
+            jpeg: "/assets/ddu_keys_active_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ddu_keys_active_768w.webp",
+            jpeg: "/assets/ddu_keys_active_768w.jpg",
+          }
+        },
+        {
+          alt: "Down Down Up Scorecard",
+          mobile: {
+            webp: "/assets/ddu_scorecard_420w.webp",
+            jpeg: "/assets/ddu_scorecard_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ddu_scorecard_768w.webp",
+            jpeg: "/assets/ddu_scorecard_768w.jpg",
+          }
+        },
+        {
+          alt: "Down Down Up in secret mode",
+          mobile: {
+            webp: "/assets/ddu_secret_mode_420w.webp",
+            jpeg: "/assets/ddu_secret_mode_420w.jpg",
+          },
+          tablet: {
+            webp: "/assets/ddu_secret_mode_768w.webp",
+            jpeg: "/assets/ddu_secret_mode_768w.jpg",
+          }
+        },
       ]
     },
   ],
