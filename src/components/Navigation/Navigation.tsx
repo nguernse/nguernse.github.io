2ca8b3b196ec68fcbceaff2b7998e7214989c56c
ng <n.guerns@gmail.com>
2024-08-07 15:35:57 -0500

    chore: update nav link color

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
   return (
     <nav className="flex bg-white p-5 rounded shadow">
-      <Link to="/" className="mx-2 hover:text-blue-500">
+      <Link to="/" className="mx-2 hover:text-blue-600">
         Home
       </Link>
 
       <ul className="ml-auto flex gap-x-2">
         <li>
-          <Link to="/about" className="mx-2 hover:text-blue-500">
+          <Link to="/about" className="mx-2 hover:text-blue-600">
             About
           </Link>
         </li>
         <li>
-          <Link to="/projects" className="mx-2 hover:text-blue-500">
+          <Link to="/projects" className="mx-2 hover:text-blue-600">
             Projects
           </Link>
         </li>
